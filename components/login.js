70a9c55944ac104a9051b5054c345c7b1d7941cb
Chris Heo <chris@avizia.com>
2018-02-01 01:08:16 -0500

    refactor

---
+++ components/login.js
@@ -0,0 +1,4 @@
+export default () => 
+    <div>
+       <h1>Welcome to CryptoEyes</h1>
+</div>
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,18 @@
-export default () => 
-<div>Welcome to next.js!
-    <p>{process.env.NODE_ENV}</p><p>{process.env.PORT || 3000}</p>
-</div>
+import Head from 'next/head'
+import Login from '../components/login'
+
+export default() => (
+        <div>
+            <Head>
+                <title>CryptoEyes</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <link rel="stylesheet" type="text/css" href="/static/semantic.min.css"></link>
+                <script
+                    src="https://code.jquery.com/jquery-3.1.1.min.js"
+                    integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+                    crossOrigin="anonymous"></script>
+                <script src="/static/semantic.min.js"></script>
+            </Head>
+            <Login />
+        </div>
+)
