f3e5e87cc23a70a81eb6dfd0851e8f2bf2b981ee
Chris Heo <chris@avizia.com>
2018-01-31 23:17:38 -0500

    add next.js with express integration

--- server/server.js
+++ server/server.js
@@ -1,12 +1,18 @@
 const express = require('express')
-const { Client } = require('pg');
-const lib = require('../lib');
-const app = express()
+const { Client } = require('pg')
+const lib = require('../lib')
+const next = require('next')
+
+const dev = process.env.NODE_ENV !== 'production'
 const port = process.env.PORT || 3000
+
+const app = express()
+const nextApp = next({ dev })
+const handle = nextApp.getRequestHandler()
 const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true,
-  })
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+})
 
 
 client.connect()
@@ -14,3 +20,22 @@ client.connect()
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log('Example app listening on port 3000!'))
+
+
+nextApp.prepare()
+  .then(() => {
+
+    app.get('/', (req, res) => {
+      return app.render(req, res, '/welcome', req.query)
+    })
+
+
+    app.get('*', (req, res) => {
+      return handle(req, res)
+    })
+
+    app.listen(port, (err) => {
+      if (err) throw err
+      console.log(`> Ready on http://localhost:${port}`)
+    })
+  })
