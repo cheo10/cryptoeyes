b09349b26d9823a93812c16455213aaa5c076f4e
Chris Heo <chris@avizia.com>
2018-01-31 21:57:25 -0500

    add config vars

--- server/server.js
+++ server/server.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 3000
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(port, () => console.log('Example app listening on port 3000!'))
